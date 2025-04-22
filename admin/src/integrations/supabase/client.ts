c3d8b34654eefe1c28f230997323721f49f142f2
Cleverson Fernandes <cleversonffaria@gmail.com>
2025-04-22 01:22:11 -0300

    ajustando env do supabase

--- admin/src/integrations/supabase/client.ts
+++ admin/src/integrations/supabase/client.ts
@@ -1,8 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const SUPABASE_URL = "https://uwinkvtamgclhfiykcso.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3aW5rdnRhbWdjbGhmaXlrY3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNjAzNTMsImV4cCI6MjA2MDczNjM1M30.vvQjPNR3JvJLv715oPpkHaf50VvEKfRwj_OsZ7LIvCQ";
- 
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
